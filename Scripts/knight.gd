89d8ad9679e5adfa4111ca38edbf11561f354604
Josiah <josmor18@kingshigh.school.nz>
2024-07-23 12:26:24 +1200

    attack animation updates
    fixing the animation where it dosent play but still in idle but it detects it.

--- Scripts/knight.gd
+++ Scripts/knight.gd
@@ -8,7 +8,7 @@ extends CharacterBody2D
 @export var attacking = false
 
 
-enum state {IDLE, RUNNING, JUMPUP, JUMPDOWN, HURT}
+enum state {IDLE, RUNNING, JUMPUP, JUMPDOWN, HURT, ATTACK}
 
 var  anim_state = state.IDLE
 
@@ -19,9 +19,6 @@ var  anim_state = state.IDLE
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var start_pos = global_position
 
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
-		attack()
 
 func reset():
 	global_position = start_pos
@@ -36,29 +33,18 @@ func update_state():
 			anim_state = state.IDLE
 		elif velocity.x != 0:
 			anim_state = state.RUNNING
-	else:
-		if velocity.y < 0:
-			anim_state = state.JUMPUP
 		else:
-			anim_state = state.JUMPDOWN
+			if velocity.y < 0:
+				anim_state = state.JUMPUP
+			else:
+				anim_state = state.JUMPDOWN
 
-func update_animation(direction):
-	if !attacking:
-		if  direction > 0:
-			animator.flip_h = false
-	elif direction < 0:
-		animator.flip_h = true
-	match anim_state:
-		state.IDLE:
-			animation_player.play("idle")
-		state.RUNNING:
-			animation_player.play("run")
-		state.JUMPUP:
-			animation_player.play("jump_up")
-		state.JUMPDOWN:
-			animation_player.play("jump_down")
-		state.HURT:
-			animation_player.play("hurt")
+
+
+
+
+
+
 
 func attack():
 	var overlapping_objects = $Attackarea.get_overlapping_areas()
@@ -66,13 +52,17 @@ func attack():
 		var parent = area.get_parent()
 		print(parent.name)
 	attacking = true
-	animation_player.play("Attack")
+	anim_state = state.ATTACK
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
+func _process(delta):
+	if Input.is_action_just_pressed("attack"):
+		attack()
+		animation_player.play("attack")
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -109,6 +99,22 @@ func _on_area_2d_body_entered(body):
 	if body.is_in_group("hurtbox"):
 		body.take_damage()
 
-
-
+func update_animation(direction):
+	if  direction > 0:
+		animator.flip_h = false
+	elif direction < 0:
+		animator.flip_h = true
+	match anim_state:
+		state.IDLE:
+			animation_player.play("idle")
+		state.RUNNING:
+			animation_player.play("run")
+		state.JUMPUP:
+			animation_player.play("jump_up")
+		state.JUMPDOWN:
+			animation_player.play("jump_down")
+		state.HURT:
+			animation_player.play("hurt")
+		state.ATTACK:
+			animation_player.play("attack")
 

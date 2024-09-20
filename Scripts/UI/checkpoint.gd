8160dcd63e6f17fbb17ea97876c820039ba8ae79
Josiah-Fren <josmor18@kingshigh.school.nz>
2024-09-20 13:02:27 +1200

    Working Sound for checkpoint
    added checkpoint line for sound

--- Scripts/UI/checkpoint.gd
+++ Scripts/UI/checkpoint.gd
@@ -1,6 +1,8 @@
 extends Node2D
 class_name Checkpoint
 @export var spawnpoint = false
+@onready var activated_cp: AudioStreamPlayer = $ActivatedCP
+
 
 var activated = false
 
@@ -19,4 +21,5 @@ func activate():
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.get_parent() is Archer && !activated:
+		activated_cp.play()
 		activate()

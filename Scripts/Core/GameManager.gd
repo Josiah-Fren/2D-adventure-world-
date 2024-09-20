507aa5a898e5b556e3614275511eaa6998d91613
Josiah-Fren <josmor18@kingshigh.school.nz>
2024-09-20 13:55:59 +1200

    Added explenations
    for the skeletons UI and flags

--- Scripts/Core/GameManager.gd
+++ Scripts/Core/GameManager.gd
@@ -14,8 +14,9 @@ func respawn_player():
 		player.position = current_checkpoint.global_position
 
 func gain_coins(coins_gained:int):
-	coins += coins_gained
-	emit_signal("gained_coins")
+	coins += coins_gained # this coin allows it to be counted from 0 to 50
+	emit_signal("gained_coins")# this give signals to when a coin is collected
 	prints(coins)
-	if coins >= 50:
+	if coins >= 50:# if there is 50 coins collected then it will trigger the ending
 		get_tree().change_scene_to_file("res://Scripts/Core/win_sreen.tscn")
+#this code allows it so that it will change to the ending screen
--- Scripts/Core/ui_manager.gd
+++ Scripts/Core/ui_manager.gd
@@ -1,9 +1,14 @@
 extends CanvasLayer
 
 func _ready():
-	GameManager.gained_coins.connect(update_coin_display)
+	GameManager.gained_coins.connect(update_coin_display) 
+	#this connects Game manager with the Ui manager so that it can update
+	#the Coin counter 
+	#When it collects a coin it turns from 0 to 1
 
 func update_coin_display():
-	$CoinDisplay.text = str(GameManager.coins)
+	$CoinDisplay.text = str(GameManager.coins) # this emits the signal from 
+	#Game manager in order to display it and update thetext for the canvas
+	# Counter
 	
 	

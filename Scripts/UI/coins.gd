ada16b11ef58b753bb39cf28a93bb96212294dfe
Josiah-Fren <josmor18@kingshigh.school.nz>
2024-09-20 12:41:01 +1200

    Coin sound
    Added coin sound when collected

--- Scripts/UI/coins.gd
+++ Scripts/UI/coins.gd
@@ -1,8 +1,8 @@
 extends Node2D
 class_name Coins
 @onready var collectedsound: AudioStreamPlayer2D = $Collectedsound
-
-
+@onready var audio = $Collectedsound
+@onready var sound = $Collectedsound
 func _ready():
 	$AnimationPlayer.play("Idle")
 	
@@ -10,7 +10,7 @@ func _ready():
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.get_parent() is Archer:
 		collectedsound.play()
-		
 		GameManager.gain_coins(1)
+		await audio.finished
 		queue_free()
 		
